1eddb3eaad17909fc2492aa0db63589201d2f72a
balatsanandrey25 <balatsanandrey25@gmail.com>
2025-11-11 15:39:28 +0600

    init server

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	server "github.com/balatsanandrey25/REST-API"
+)
+
+const (
+	port = "8080"
+)
+
+func main() {
+	srv := new(server.Server)
+	if err := srv.Run(port); err != nil {
+		log.Fatalf("error occured while runnig http server %s", err.Error())
+	}
+}
